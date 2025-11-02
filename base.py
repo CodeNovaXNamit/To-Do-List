74dd0a419acb19eefc22f8c4be42a4bf57f44495
CodeNovaXNamit <namit.profession@gmail.com>
2025-11-02 13:24:52 +0530

    a

--- base.py
+++ base.py
@@ -1,1 +1,5 @@
 #file for to do list app
+print("This is the To-Do-List app made by Namit Bhardwaj")
+def add_task(task_list, task):
+    task_list.append(task)
+    print(f'Task "{task}" added to the list.')
